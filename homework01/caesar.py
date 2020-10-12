271b696a82dc29e89b26de46366f584de6e7ad75
mamoudou-tr35 <mamoudoudiakatraore@gmail.com>
2020-10-12 16:55:48 +0300

    ncnnffjjfj

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -14,9 +14,15 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    ciphertext = "" 
+    for c in plaintext:
+        if (ord(c) <= 64) or (91 <= ord(c) <= 96) or (123 <= ord(c)):
+            ciphertext += chr(ord(c))
+        elif (97 <= ord(c) + 3 <= 122) or (65 <= ord(c) + 3 <= 90):
+            ciphertext += chr(ord(c) + 3)
+        else:
+            ciphertext += chr(ord(c) + 3 - 26)
+     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
@@ -33,13 +39,22 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for c in ciphertext:
+        if (ord(c) <= 64) or (91 <= ord(c) <= 96) or (123 <= ord(c)):
+            plaintext += chr(ord(c))
+        elif (97 <= ord(c) - 3 <= 122) or (65 <= ord(c) - 3 <= 90):
+            plaintext += chr(ord(c) - 3)
+        else:
+            plaintext += chr(ord(c) - 3 + 26)
     return plaintext
 
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
+def caesar_breaker(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
-    Brute force breaking a Caesar cipher.
+    >>> d = {"python", "java", "ruby"}
+    >>> caesar_breaker("python", d)
+    0
+    >>> caesar_breaker("sbwkrq", d)
+    3
     """
     best_shift = 0
     # PUT YOUR CODE HERE
