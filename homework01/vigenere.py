2f7b6f53790f4021e5b309dd0535fe4ccf0cc0d1
mamoudou-tr35 <mamoudoudiakatraore@gmail.com>
2020-10-17 18:27:37 +0300

    Реализована функция def encrypt_vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,9 +9,32 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+    letters_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letters_lowercase = "abcdefghijklmnopqrstuvwxyz"
+    a = 26
+
+    while len(keyword) < len(plaintext):
+        keyword += keyword
+
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    for i in range(len(plaintext)):
+        pc = ord(plaintext[i])
+
+        if keyword[i] in letters_uppercase:
+            N = ord(keyword[i]) - ord("A")
+        elif keyword[i] in letters_lowercase:
+            N = ord(keyword[i]) - ord("a")
+
+        if (97 <= pc + N <= 122) and pc > 96 and pc < 123 \
+                or (65 <= pc + N <= 90) and pc > 64 and pc < 91:
+            ciphertext += chr(pc + N)
+        else:
+            ciphertext += chr(pc + N - a)
+
     return ciphertext
+
+
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -25,6 +48,27 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
+    letters_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letters_lowercase = "abcdefghijklmnopqrstuvwxyz"
+    a = 26
+
+    while len(keyword) < len(ciphertext):
+        keyword += keyword
+
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    for i in range(len(ciphertext)):
+        ct = ord(ciphertext[i])
+
+        if keyword[i] in letters_uppercase:
+            K = ord(keyword[i]) - ord("A")
+        elif keyword[i] in letters_lowercase:
+            K = ord(keyword[i]) - ord("a")
+
+        if (97 <= ct - K <= 122) and ct > 96 and ct < 123 \
+                or (65 <= ct - K <= 90) and ct > 64 and ct < 91:
+            plaintext += chr(ct - K)
+        else:
+            plaintext += chr(ct - K + a)
     return plaintext
+
